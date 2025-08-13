431bdd4144ebccb066c207024359d2be6cf66a2b
Lucas Freitas <lucasfreitasgump@gmail.com>
2025-08-12 21:07:56 -0300

    Finalizado exercício 17

--- aula014ex/ex017/scripts/script.js
+++ aula014ex/ex017/scripts/script.js
@@ -1,1 +1,21 @@
-alert('Ola')
+function gerarTabuada(){
+    let numero = document.getElementById('numero')
+    let tabuada = document.getElementById('tabuada')
+
+
+    if(numero.value.length == 0){
+        alert('Insira um número.')
+        
+    }else{
+        numero = Number(numero.value)
+        tabuada.innerHTML = ''
+        
+        for(let i = 1; i < 11; i++){
+            let item = document.createElement('option')
+            item.text = `${i} x ${numero} = ${i*numero}`
+            tabuada.appendChild(item)
+        }
+    }
+    
+    
+}
