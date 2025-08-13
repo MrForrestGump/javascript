6df254574cc12b49c36cf99c72780589d85f4de8
Lucas Freitas <lucasfreitasgump@gmail.com>
2025-08-13 16:17:24 -0300

    criação aula 17

---
+++ aula17/objeto01.js
@@ -0,0 +1,12 @@
+let amigo = {nome: 'Lucas', sexo: 'Masculino', peso: 73.5}
+
+console.log(amigo)
+console.log(amigo.nome)
+
+function engordar(p){
+    console.log('Engordou')
+    this.peso += 10
+    console.log(`agora pesa ${p.peso}Kg`)
+}
+
+engordar(amigo)
